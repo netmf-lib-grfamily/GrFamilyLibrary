47ab29f549ea85d8b21e2a1b2894642b3113bc66
seosoft <y.seo@seosoft.jp>
2015-10-10 22:21:51 +0900

    キャラクターディスプレイのライブラリ初期バージョン

--- TestApp/Module/LiquidCrystalTest/Program.cs
+++ TestApp/Module/LiquidCrystalTest/Program.cs
@@ -11,6 +11,8 @@ namespace LiquidCrystalTest
     {
         public static void Main()
         {
+            Thread.Sleep(1000);
+
             // 開発時の標準ピンアサイン
             var lcd = new LiquidCrystal(Pins.GPIO_PIN_D9, Pins.GPIO_PIN_D8,
                 Pins.GPIO_PIN_D7, Pins.GPIO_PIN_D6, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D4);
@@ -18,9 +20,13 @@ namespace LiquidCrystalTest
             //var lcd = new LiquidCrystal(Pins.GPIO_PIN_D9, Pins.GPIO_PIN_D8,
             //    Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D2, Pins.GPIO_PIN_D1, Pins.GPIO_PIN_D0);
 
+            // GR-PEACHではコマンド間のウェイトなし動作する（ウェイトありでも問題ない）
+            // GR -SAKURAではウェイトを入れないと誤動作する
+            lcd.InitDevice(5);
+
             while (true)
             {
-                lcd.InitDevice();
+                lcd.SetCursor(0, 0);
                 lcd.Print("GRFamily Library");
                 lcd.SetCursor(1, 0);
                 lcd.Print("LiquidCrystal !!");
