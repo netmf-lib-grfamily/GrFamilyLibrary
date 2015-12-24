783e771a40ef807153ce6424c5a1cf228511dc56
seosoft <y.seo@seosoft.jp>
2015-12-24 23:12:50 +0900

    NTP Time Server から日時を取得するメソッドのコードを整理

--- Library/Utility/NetworkUtility/SystemTimeInitializer.cs
+++ Library/Utility/NetworkUtility/SystemTimeInitializer.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Time;
 
 namespace GrFamily.Utility
 {
@@ -32,7 +30,8 @@ namespace GrFamily.Utility
         /// <param name="ntpServer">NTP サーバー</param>
         /// <param name="timezoneOffset">時差 (単位：分)</param>
         /// <remarks>情報源<br />
-        /// http://weblogs.asp.net/mschwarz/wrong-datetime-on-net-micro-framework-devices </remarks>
+        /// http://stackoverflow.com/questions/1193955/how-to-query-an-ntp-server-using-c<br />
+        /// http://weblogs.asp.net/mschwarz/wrong-datetime-on-net-micro-framework-devices</remarks>
         public static void InitSystemTime(string ntpServer, int timezoneOffset)
         {
             var ep = new IPEndPoint(Dns.GetHostEntry(ntpServer).AddressList[0], 123);
@@ -40,31 +39,26 @@ namespace GrFamily.Utility
             var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             sock.Connect(ep);
 
-            var ntpData = new byte[48]; // RFC 2030
+            var ntpData = new byte[48];
             ntpData[0] = 0x1b;
             for (var i = 1; i < 48; i++)
                 ntpData[i] = 0;
 
             sock.Send(ntpData);
             sock.Receive(ntpData);
-
-            var offsetTransmitTime = 40;
-            ulong intpart = 0;
-            ulong fractpart = 0;
-            for (var i = 0; i <= 3; i++)
-                intpart = 256 * intpart + ntpData[offsetTransmitTime + i];
-
-            for (var i = 4; i <= 7; i++)
-                fractpart = 256 * fractpart + ntpData[offsetTransmitTime + i];
-
-            ulong milliseconds = (intpart * 1000 + (fractpart * 1000) / 0x100000000L);
-
             sock.Close();
 
-            TimeSpan timeSpan = TimeSpan.FromTicks((long)milliseconds * TimeSpan.TicksPerMillisecond);
-            DateTime dateTime = new DateTime(1900, 1, 1);
-            dateTime += timeSpan;
+            const int offset = 40;
+            ulong intPart = 0;
+            for (var i = 0; i <= 3; i++)
+                intPart = 256 * intPart + ntpData[offset + i];
 
+            ulong fractPart = 0;
+            for (var i = 4; i <= 7; i++)
+                fractPart = 256 * fractPart + ntpData[offset + i];
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+            var dateTime = new DateTime(1900, 1, 1).AddMilliseconds(milliseconds);
             var networkDateTime = dateTime + new TimeSpan(0, timezoneOffset, 0);
 
             Microsoft.SPOT.Hardware.Utility.SetLocalTime(networkDateTime);
